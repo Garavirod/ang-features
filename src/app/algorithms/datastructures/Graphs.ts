7c483e1d849383fa49345edcfb8b610d7bc28386
Garavirod <rodrigogarciaavila26@gmail.com>
2021-03-01 17:00:06 -0600

    edgelist gepah implememntation was added

--- src/app/algorithms/datastructures/Graphs.ts
+++ src/app/algorithms/datastructures/Graphs.ts
@@ -5,12 +5,16 @@ export interface Graph{
 }
 
 
+/* 
+    ----    ADYANCENT MATRIX ----
+*/
+
 //Non ponderated graph
 export class AdyacentNonPonMatrixGraph implements Graph{
 
     matrix:Array<Array<any>>;
-    constructor(v:number){
-        this.matrix = new Array(v).fill(0).map(() => new Array(v).fill(0));        
+    constructor(numVertex:number){
+        this.matrix = new Array(numVertex).fill(0).map(() => new Array(numVertex).fill(0));        
     }
 
     //Override
@@ -25,12 +29,12 @@ export class AdyacentNonPonMatrixGraph implements Graph{
     }
 }
 
-
+//Ponderated graph
 export class AdyacentPonMatGraph implements Graph{
 
      matrix:Array<Array<any>>;
-     constructor(v:number){
-         this.matrix = new Array(v).fill(0).map(() => new Array(v).fill(0));        
+     constructor(numVertex:number){
+         this.matrix = new Array(numVertex).fill(0).map(() => new Array(numVertex).fill(0));        
      }
  
      //Override
@@ -45,3 +49,68 @@ export class AdyacentPonMatGraph implements Graph{
      }
 }
 
+/* 
+    ----    ADYANCENT LIST ----
+*/
+
+/* 
+    It is an array and each item it is a vertex
+    arr = [v1(idx), v2(idx), ...., vn(idx)]
+    v(1) = arr[ v1, v3, v6]
+    v(2) = arr[ v1, v3, v6]
+    v(3) = arr[ v1, v8]
+    Adayancece list is an array where each index represent the graph vertex
+    and the cotentent of that index is another array that is felt with 
+    a list of vertexes connected to it.
+*/
+
+
+export class AdyacentListGraph implements Graph{
+    /* Override */
+    matrix:Array<any>;
+
+    constructor(numVertex:number){
+        this.matrix = new Array(numVertex).fill(null);
+        
+    }
+
+    /* Override */
+    fillGraph(vertex:number, vertexes:Array<any>):void{
+        this.matrix[vertex-1] = vertexes;
+        this.printGraph();
+    }
+    /* Override */
+    printGraph():void{
+        console.log(this.matrix);        
+    }
+}
+
+
+/* 
+    ----    EDGE LIST ----
+*/
+
+/* 
+    arr = [e1, e2, ....., en]
+    It is an array and each item is an edge, and its content is a pair of vertexes,
+    this way: e = (vi, vj).
+*/
+
+export class EdgetListGraph implements Graph{
+    /* Override */
+    matrix:Array<any>;
+
+    constructor(numEdges:number){
+        this.matrix = new Array(numEdges).fill(null);        
+    }
+
+    /* Override */
+    fillGraph(edge:number, vertexes:Array<any>):void{
+        this.matrix[edge-1] = vertexes;
+        this.printGraph();
+    }
+    /* Override */
+    printGraph():void{
+        console.log(this.matrix);        
+    }
+}
--- src/app/algorithms/views/graphs/graphs.component.ts
+++ src/app/algorithms/views/graphs/graphs.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AdyacentNonPonMatrixGraph, Graph, AdyacentPonMatGraph } from '../../datastructures/Graphs';
+import { AdyacentNonPonMatrixGraph, Graph, AdyacentPonMatGraph, AdyacentListGraph } from '../../datastructures/Graphs';
 
 @Component({
   selector: 'app-graphs',
@@ -9,9 +9,11 @@ import { AdyacentNonPonMatrixGraph, Graph, AdyacentPonMatGraph } from '../../dat
 export class GraphsComponent implements OnInit {
   graph1:Graph;
   graph2:Graph;
+  graph3:Graph;
   constructor() { 
     this.graph1 =  new AdyacentNonPonMatrixGraph(7);    
     this.graph2 = new AdyacentPonMatGraph(5);
+    this.graph3 = new AdyacentListGraph(9);
   }
 
   ngOnInit(): void {
@@ -19,6 +21,8 @@ export class GraphsComponent implements OnInit {
     this.graph1.fillGraph(3,5);
     console.log(".....PONDERATED GRPAH....");
     this.graph2.fillGraph(4,2,12);
+    console.log(".....NON PONDERATED LIST ADYACENT GRPAH....");
+    this.graph3.fillGraph(4,[3,1,5]);
   }
 
 }
